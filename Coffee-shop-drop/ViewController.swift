2a79c7806a3106fe7a45116afb98263c19f28eb7
Sam <samuelburke404@gmail.com>
2018-12-11 14:38:08 +0000

    Initial Commit

---
+++ Coffee-shop-drop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee-shop-drop
+//
+//  Created by CodeNation18 on 11/12/2018.
+//  Copyright © 2018 CodeNation18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Coffee-shop-dropTests/Coffee_shop_dropTests.swift
@@ -0,0 +1,36 @@
+//
+//  Coffee_shop_dropTests.swift
+//  Coffee-shop-dropTests
+//
+//  Created by CodeNation18 on 11/12/2018.
+//  Copyright © 2018 CodeNation18. All rights reserved.
+//
+
+import XCTest
+@testable import Coffee_shop_drop
+
+class Coffee_shop_dropTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
